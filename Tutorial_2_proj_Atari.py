d06122c1c95bc787c98203492597584fe0c07f88
Andrea <andrea.monguzzi@polimi.it>
2024-04-05 11:45:42 +0200

    tutorial 2 fixed. added tutorial 3

--- Tutorial_2_proj_Atari.py
+++ Tutorial_2_proj_Atari.py
@@ -8,17 +8,22 @@ import os
 
 Test_env = False
 Train_agent = False
+Use_the_agent = True
 
 ##########################
 ## Load the environment ##
 ##########################
 
-environment_name = 'Breakout-v4'
-env = gym.make(environment_name, render_mode='human')
-env.metadata['render_fps'] = 30 # set fps for rendering
+environment_name = 'Breakout-v0'
 
-print(env.observation_space)
-print(env.action_space)
+env0 = gym.make(environment_name, render_mode='human')
+env0.metadata['render_fps'] = 30 # set fps for rendering
+
+print(env0.observation_space)
+print(env0.action_space)
+
+# The observation space is a Box space with shape (210, 160, 3). This means that the observation is an image with a height of 210 pixels, a width of 160 pixels, and three color channels (RGB). 
+# The action space is a Discrete space with 4 actions. This means that the agent can take one of four actions: 0 (no operation), 1 (fire), 2 (move right), and 3 (move left).
 
 ##########################
 ## Test the environment ##
@@ -29,18 +34,18 @@ if Test_env:
    
     episodes = 5
     for episode in range(episodes+1):
-        obs = env.reset()
+        obs = env0.reset()
         done = False
         score = 0
 
         while not done:
-            env.render()
-            action = env.action_space.sample()
-            obs, reward, done, trun, info = env.step(action)
+            env0.render()
+            action = env0.action_space.sample()
+            obs, reward, done, trun, info = env0.step(action)
             score += reward
 
         print(f'Episode: {episode} Score: {score}')
-        env.close()
+        env0.close()
 
 ###############################
 ## Vectorise the environment ##
@@ -48,8 +53,13 @@ if Test_env:
 
 # Note: in Tutorial_1 we did not vectorize the environment, here we do (VecFrameStack). This allows to run multiple environments in parallel, speeding up training (TRAIN IN PARALLEL)
 # To play with 4 different env at once:
-env = make_atari_env(environment_name, n_envs=4, seed=0) # This function is used to create a vectorized environment for Atari games. The environment_name parameter specifies which Atari game to use. The n_envs=4 parameter indicates that four parallel environments should be created. The seed=0 parameter sets the seed for the random number generator in the environment, ensuring that the results are reproducible.
-env = VecFrameStack(env, n_stack=4) # stack envs together
+env4 = make_atari_env(environment_name, n_envs=4, seed=0) # This function is used to create a vectorized environment for Atari games. The environment_name parameter specifies which Atari game to use. The n_envs=4 parameter indicates that four parallel environments should be created. The seed=0 parameter sets the seed for the random number generator in the environment, ensuring that the results are reproducible.
+#env4.metadata['render_modes'] = ["rgb_array"] # set render modes
+env4.metadata['render_fps'] = 30 # set fps for rendering
+env4 = VecFrameStack(env4, n_stack=4) # stack envs together
+
+env4.reset()
+env4.render('human')
 
 
 #################
@@ -58,15 +68,15 @@ env = VecFrameStack(env, n_stack=4) # stack envs together
 
 log_path = os.path.join('Training', 'Logs')
 A2C_path = os.path.join('Training', 'Saved Models', 'A2C_Model_Breakout')
-model = A2C('CnnPolicy', env, verbose=1, tensorboard_log=log_path) # The A2C algorithm is used to train the model. The 'CnnPolicy' parameter specifies that a convolutional neural network is used since the obs are images
+model = A2C('CnnPolicy', env4, verbose=1, tensorboard_log=log_path) # The A2C algorithm is used to train the model. The 'CnnPolicy' parameter specifies that a convolutional neural network is used since the obs are images
 
 if Train_agent:
     print('Training the agent')
-    model.learn(total_timesteps=100000)
+    model.learn(total_timesteps=200000)
     model.save(A2C_path)
 else:
     print('Loading the agent')
-    model = A2C.load(A2C_path, env=env)
+    model = A2C.load(A2C_path, env=env4)
 
 
 ########################
@@ -75,14 +85,32 @@ else:
 
 print('Evaluating the model')
 # To evaluate the model we have to define a env that is not vectorized, but need to be stacked
-env = make_atari_env(environment_name, n_envs=1, seed=0)
-env = VecFrameStack(env, n_stack=4)
+env1 = make_atari_env(environment_name, n_envs=1, seed=0)
+#env1.metadata['render_modes'] = ["rgb_array"] # set render modes
+env1.metadata['render_fps'] = 30 # set fps for rendering
+env1 = VecFrameStack(env1, n_stack=4)
 
-env.metadata['render_fps'] = 30 # set fps for rendering
-env.metadata['render_modes'] = 'human'
+# env.metadata['render_fps'] = 120 # set fps for rendering
+# env.metadata['render_modes'] = 'human'
 
-mean_rew, std_rew = evaluate_policy(model, env, n_eval_episodes=10, render=True) # The evaluate_policy function is used to evaluate the model. The model parameter specifies the model to evaluate. The env parameter specifies the environment to use. The n_eval_episodes=10 parameter specifies that the model should be evaluated over ten episodes. The render=True parameter specifies that the evaluation should be rendered to the screen.
+mean_rew, std_rew = evaluate_policy(model, env1, n_eval_episodes=10, render=True) # The evaluate_policy function is used to evaluate the model. The model parameter specifies the model to evaluate. The env parameter specifies the environment to use. The n_eval_episodes=10 parameter specifies that the model should be evaluated over ten episodes. The render=True parameter specifies that the evaluation should be rendered to the screen.
 print(f'Mean reward: {mean_rew} +/- {std_rew}')
 
 # Note: in this case you can see that the model is not performing well. This is because the model is not trained for long enough. To improve the performance of the model, you can train it for more timesteps.
 
+if Use_the_agent:
+    print('Using the agent')
+    episodes = 100
+    for episode in range(1, episodes+1):
+        obs = env1.reset()
+        done = False
+        score = 0
+
+        while not done:
+            env1.render('human')
+            action, _ = model.predict(obs) # The model predicts the action to take based on the observation. output: action and the value of the next state (used in recurrent policies)
+            obs, reward, done, info = env1.step(action) # The environment takes the action and returns the new observation, the reward, if the episode is done, and additional information
+            score += reward
+            
+        print(f'Episode: {episode}, Score: {score}')
+    env1.close()
--- Tutorial_3_proj_Driving.py
+++ Tutorial_3_proj_Driving.py
@@ -4,7 +4,10 @@ from stable_baselines3.common.vec_env import DummyVecEnv
 from stable_baselines3.common.evaluation import evaluate_policy
 import os
 
-Test_env = True
+Test_env = False
+Train_model = False
+Use_agent = False
+Evaluate_model = True
 
 ##########################
 ## Load the environment ##
@@ -13,9 +16,19 @@ Test_env = True
 environmant_name = 'CarRacing-v2'
 env = gym.make(environmant_name, render_mode='human')
 
-print(env.observation_space)
-print(env.action_space)
+#The easiest control task to learn from pixels - a top-down racing environment. The generated track is random every episode.
+#Some indicators are shown at the bottom of the window along with the state RGB buffer. From left to right: 
+#true speed, four ABS sensors, steering wheel position, and gyroscope. 
 
+print(env.observation_space)
+# State consists of 96x96 rgb pixels (values from 0 to 255). The car is centered in the field of view, can move 3 directions, and can rotate.
+print(env.action_space)
+# continuous: There are 3 actions: steering (-1 is full left, +1 is full right), gas, and breaking. Stearing goes from -1 to 1, gas from 0 to 1, and breaking from 0 to 1.
+
+# The reward is -0.1 every frame and +1000/N for every track tile visited, where N is the total number of tiles visited in the track. For example, if you have finished in 
+# 732 frames, your reward is 1000 - 0.1*732 = 926.8 points.
+# The episode finishes when all of the tiles are visited. The car can also go outside of the playfield - that is, far off the track, in which case it will receive -100 reward and die.
+# Game is solverd when reward is greater than 900 points.
 
 ##################
 ## Test the env ##
@@ -35,7 +48,61 @@ if Test_env:
             action = env.action_space.sample()
             obs, reward, done, trun, info = env.step(action)
             score += reward
+            print(f'Action: {action}')
+            print(f'Observation: {obs}')
+            print(f'Reward: {reward}')
 
         print(f'Episode: {episode} Score: {score}')
         env.close()
     
+
+#####################
+## Train the model ##
+#####################
+
+log_path = os.path.join('Training', 'Logs')
+PPO_path = os.path.join('Training', 'Saved Models', 'PPO_Model_Driving')
+
+env1 = gym.make(environmant_name, render_mode='rgb_array') # rgb otherways it will plot the image while training
+envd= DummyVecEnv([lambda: env1])
+
+
+if Train_model:
+    print('Training the model')
+    model = PPO('CnnPolicy', envd, verbose=1, tensorboard_log=log_path) # image input -> convolutional neural network
+    model.learn(total_timesteps=200000)
+    model.save(PPO_path)
+else:
+    print('Loading the model')
+    model = PPO.load(PPO_path, env =envd)
+
+
+###################
+## Use the model ##
+###################
+
+if Use_agent:
+    print('Using the agent')
+    episodes = 10
+    for episode in range(1, episodes+1):
+        obs = envd.reset()
+        done = False
+        score = 0
+
+        while not done:
+            envd.render()
+            action, _ = model.predict(obs) # The model predicts the action to take based on the observation. output: action and the value of the next state (used in recurrent policies)
+            obs, reward, done, info = envd.step(action) # The environment takes the action and returns the new observation, the reward, if the episode is done, and additional information
+            score += reward
+            
+        print(f'Episode: {episode}, Score: {score}')
+    envd.close()
+
+########################
+## Evaluate the model ##
+########################
+
+if Evaluate_model:
+    print('Evaluating the model')
+    mean_rew, std_rew = evaluate_policy(model, env, n_eval_episodes=10, render=True) # Evaluate the model for 10 episodes and render the environment
+    print(f'Mean reward: {mean_rew}, Std reward: {std_rew}')
