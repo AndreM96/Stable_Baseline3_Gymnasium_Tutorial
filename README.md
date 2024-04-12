c06f41c3f1593bdba2614362feffca715760f33a
Andrea <andrea.monguzzi@polimi.it>
2024-04-12 11:57:49 +0200

    simple tutorial 5 added. to do: tutorial 6

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Useful links:
 Tutorial 1, 2, 3, 4 inspired by:
 https://www.youtube.com/watch?v=Mut_u40Sqz4&t=5197s 
 
-Main Requirements:
+Requirements (creating a virtual environment is recommended):
 - Python 3.8+
 - Stable baseline 3:
     pip install stable-baselines3[extra]
@@ -13,7 +13,10 @@ Main Requirements:
 - Gymnasium atari:
      - pip install gymnasium[atari]
      - pip install gymnasium[accept-rom-license]
+- Gymnasium box 2d:
      - pip install gymnasium[box2d]
+- Gymnasium robotics:
+     - pip install gymnasium-robotics
 - Swig:
      apt-get install swig
 
---
+++ Tutorial_5_gymnasium_Robotics.py
@@ -0,0 +1,16 @@
+# Gymnsaium Robotics includes a set of environments for robotic manipulation tasks based on mujoco.
+# Algorithm from stable baseline 3 can be used to solve these tasks (see previous tutorial). 
+# This is a simple tutorial that shows how to load and test and environment from Gymnasium Robotics.
+
+import gymnasium as gym
+
+env = gym.make("FetchPickAndPlace-v2", render_mode="human")
+observation, info = env.reset(seed=42)
+
+for _ in range(1000):
+   action = env.action_space.sample()  # User-defined policy function
+   observation, reward, terminated, truncated, info = env.step(action)
+
+   if terminated or truncated:
+      observation, info = env.reset()
+env.close()
