d874a0569a76c2f4c74faaabc1cbffc29c232140
Andrea <andrea.monguzzi@polimi.it>
2024-04-13 14:46:00 +0200

    fixed tutorial 5

--- Tutorials/Tutorial_5_gymnasium_Robotics.py
+++ Tutorials/Tutorial_5_gymnasium_Robotics.py
@@ -8,7 +8,7 @@ env = gym.make("FetchPickAndPlace-v2", render_mode="human")
 observation, info = env.reset(seed=42)
 
 for _ in range(1000):
-   action = env.action_space.sample()  # User-defined policy function
+   action = env.action_space.sample()  # to replace with a User-defined policy function
    observation, reward, terminated, truncated, info = env.step(action)
 
    if terminated or truncated:
